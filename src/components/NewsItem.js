868338fc6950021e9a5d251bf46c88d07b21c49e
AliHassan154 <codewithali9@gmail.com>
2026-05-09 01:37:31 -0700

    ci cd pipeline

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-            <img className="card-img-top" src={imageurl} alt="Card image cap"/>
+            <img className="card-img-top" src={imageurl} alt={title}/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
